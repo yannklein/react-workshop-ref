feaac62b0f005809c0a87aa73d84760c64036299
Yann Klein <yann.klein@me.com>
2024-03-10 13:14:29 +0900

    add rule for semi colons

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,24 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import CafeList from './components/CafeList/CafeList';
-import Slides from './components/Slides/Slides';
-
 
 function App() {
   const [cafes, setCafes] = React.useState([]);
   const [keyword, setKeyword] = React.useState("");
-  const [slidesDisplayed, setSlidesDisplayed] = React.useState(false);
 
   React.useEffect(() => {
-    const url = "https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes"
-    const params = (keyword !== "") ? `?title=${keyword}` : ""
+    const url = "https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes";
+    const params = (keyword !== "") ? `?title=${keyword}` : "";
     fetch(url + params)
       .then(response => response.json())
       .then((data) => {
         setCafes(data);
-      })
+      });
   }, [keyword]);
 
   return (
     <div className='app-frame'>
-      <Slides slidesDisplayed={slidesDisplayed} setSlidesDisplayed={setSlidesDisplayed} />
-      <Navbar setKeyword={setKeyword} setSlidesDisplayed={setSlidesDisplayed}/>
+      <Navbar setKeyword={setKeyword}/>
       <div className="app-body">
         <Sidebar setCafes={setCafes} />
         <CafeList cafes={cafes}/>
